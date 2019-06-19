b51c0cbdbb43fca679aeb6f29103c15d4dbe043e
Diviyansh Bhatnagar <ansh.divu@gmail.com>
2019-06-19 00:25:10 -0400

    added `add new item` feature

--- src/line-item.tsx
+++ src/line-item.tsx
@@ -1,53 +1,74 @@
 import React, { useState } from 'react';
 import Item from './core/line-item';
 
-type OnChangeFn = (item: Item) => void;
+type OnChangeProp = (item: Item) => void;
 
 function useLineItem(initialItem = new Item()) {
   const [item, setItem] = useState(initialItem);
 
-  const updateItem = (update: Partial<Item>) =>
-    setItem(new Item({ ...item, ...update }));
+  const updateItem = (update: Partial<Item>, onChange?: OnChangeProp) =>
+    setItem(prev => {
+      const updatedItem = new Item({ ...prev, ...update });
+      onChange && onChange(updatedItem);
+      return updatedItem;
+    });
 
   return [item, updateItem] as const;
 }
 
-export default function LineItem({ onChange }: { onChange?: OnChangeFn }) {
-  const [item, updateItem] = useLineItem();
-  onChange && onChange(item);
+export default function LineItem({
+  item: initialValue,
+  onChange
+}: {
+  item: Item;
+  onChange?: OnChangeProp;
+}) {
+  const [item, updateItem] = useLineItem(initialValue);
 
   return (
     <div className="bt b--black-50">
-      <label className="fl w-25">
+      <label className="fl w-10">
+        Delete
+        <button>
+          <span
+            role="img"
+            aria-label="Delete"
+            onClick={() => alert('finish delete feature')}
+          >
+            ❌
+          </span>
+        </button>
+      </label>
+      <label className="fl w-30">
         Description:
         <input
           type="text"
           value={item.description}
-          onChange={e => updateItem({ description: e.target.value })}
+          onChange={e => updateItem({ description: e.target.value }, onChange)}
         />
       </label>
-      <label className="fl w-25">
+      <label className="fl w-20">
         Optimistic:
         <input
           type="number"
           value={item.optimistic}
-          onChange={e => updateItem({ optimistic: +e.target.value })}
+          onChange={e => updateItem({ optimistic: +e.target.value }, onChange)}
         />
       </label>
-      <label className="fl w-25">
+      <label className="fl w-20">
         Likely:
         <input
           type="number"
           value={item.likely}
-          onChange={e => updateItem({ likely: +e.target.value })}
+          onChange={e => updateItem({ likely: +e.target.value }, onChange)}
         />
       </label>
-      <label className="fl w-25">
+      <label className="fl w-20">
         Pessimistic:
         <input
           type="number"
           value={item.pessimistic}
-          onChange={e => updateItem({ pessimistic: +e.target.value })}
+          onChange={e => updateItem({ pessimistic: +e.target.value }, onChange)}
         />
       </label>
     </div>
--- src/project.tsx
+++ src/project.tsx
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import LineItem from './line-item';
+import Item from './core/line-item';
 
 export default function Project() {
+  let [items, setItems] = useState([new Item()]);
+
+  const updateItem = (idx: number) => (updatedItem: Item) =>
+    setItems(prev => {
+      const newItems = [...prev];
+      newItems[idx] = updatedItem;
+      return newItems;
+    });
+
+  const addNewItem = () => setItems(prev => prev.concat([new Item()]));
+
   return (
     <>
-      <LineItem onChange={console.log} />
-      <input type="submit" value="Submit" />
+      {items.map((item, idx) => (
+        <LineItem item={item} key={idx} onChange={updateItem(idx)} />
+      ))}
+      <button onClick={addNewItem}>New Item</button>
     </>
   );
 }

315e702c1b43be75ff013b9fb0c860f509a869e5
Diviyansh Bhatnagar <ansh.divu@gmail.com>
2019-07-01 22:15:33 -0400

    delete functionality working

--- src/project.tsx
+++ src/project.tsx
@@ -1,23 +1,69 @@
 import React, { useState } from "react";
-import LineItem from "./line-item";
 import Item from "./core/line-item";
 
 export default function Project() {
   let [items, setItems] = useState([new Item()]);
 
-  const updateItem = (idx: number) => (updatedItem: Item) =>
+  const updateItem = (idx: number, update: Partial<Item>) =>
     setItems(prev => {
       const newItems = [...prev];
-      newItems[idx] = updatedItem;
+      newItems[idx] = new Item({ ...newItems[idx], ...update });
       return newItems;
     });
 
   const addNewItem = () => setItems(prev => prev.concat([new Item()]));
+  const deleteItem = (idx: number) => () =>
+    setItems(prev => {
+      const newItems = [...prev];
+      newItems.splice(idx, 1);
+      return newItems;
+    });
 
   return (
     <>
       {items.map((item, idx) => (
-        <LineItem item={item} key={idx} onChange={updateItem(idx)} />
+        <div key={idx} className="bt b--black-50">
+          <label className="fl w-10">
+            Delete
+            <button>
+              <span role="img" aria-label="Delete" onClick={deleteItem(idx)}>
+                ❌
+              </span>
+            </button>
+          </label>
+          <label className="fl w-30">
+            Description:
+            <input
+              type="text"
+              value={item.description}
+              onChange={e => updateItem(idx, { description: e.target.value })}
+            />
+          </label>
+          <label className="fl w-20">
+            Optimistic:
+            <input
+              type="number"
+              value={item.optimistic}
+              onChange={e => updateItem(idx, { optimistic: +e.target.value })}
+            />
+          </label>
+          <label className="fl w-20">
+            Likely:
+            <input
+              type="number"
+              value={item.likely}
+              onChange={e => updateItem(idx, { likely: +e.target.value })}
+            />
+          </label>
+          <label className="fl w-20">
+            Pessimistic:
+            <input
+              type="number"
+              value={item.pessimistic}
+              onChange={e => updateItem(idx, { pessimistic: +e.target.value })}
+            />
+          </label>
+        </div>
       ))}
       <button onClick={addNewItem}>New Item</button>
     </>
